4922a9bd0b3d318fab9cb519af43bc9621f500ce
maasencioh <maasencioh@gmail.com>
2016-11-22 17:14:31 -0500

    feat: read data from agilent format

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 'use strict';
 
-const defaultOptions = {};
+const NetCDFReader = require('netcdfjs');
+const agilent = require('./agilent');
 
-function netcdfGcms(options) {
-    options = Object.assign({}, defaultOptions, options);
+function netcdfGcms(data) {
+    let reader = new NetCDFReader(data);
+    const globalAttributes = reader.globalAttributes;
+
+    if (globalAttributes.find(val => val.name === 'dataset_origin')) {
+        return agilent(reader);
+    } else if (globalAttributes.find(val => val.name === 'source_file_format')) {
+        throw new TypeError('Finnigan format not implemented yet');
+    } else {
+        throw new TypeError('Unknown file format');
+    }
+}
+
+function fromAgilent(data) {
+    return agilent(new NetCDFReader(data));
 }
 
 module.exports = netcdfGcms;
+module.exports.fromAgilent = fromAgilent;
--- test/test.js
+++ test/test.js
@@ -1,10 +1,48 @@
 'use strict';
 
 const netcdfGcms = require('..');
+const fs = require('fs');
+const pathFiles = __dirname + '/files/';
 
 describe('netcdf-gcms test', function () {
 
-    it('Something to test', function () {
-        (true).should.be.equal(true);
+    it('Unknown file format', function () {
+        const data = fs.readFileSync(pathFiles + 'madis-sao.nc');
+        netcdfGcms.bind(null, data).should.throw('Unknown file format');
+    });
+
+    describe('Agilent format', function () {
+        it('Agilent file', function () {
+            const data = fs.readFileSync(pathFiles + 'P071.CDF');
+            const json = netcdfGcms(data);
+            //console.log(json);
+            json.times.length.should.be.equal(6401);
+            for (let i = 0; i < json.series.length; i++) {
+                json.series[i].data.length.should.be.equal(6401);
+            }
+        });
+
+        it('fromAgilent', function () {
+            const data = fs.readFileSync(pathFiles + 'P071.CDF');
+            const json = netcdfGcms.fromAgilent(data);
+            json.times.length.should.be.equal(6401);
+            for (let i = 0; i < json.series.length; i++) {
+                json.series[i].data.length.should.be.equal(6401);
+            }
+        });
+    });
+
+    describe('Finnigan format', function () {
+        it('Finnigan file', function () {
+            const data = fs.readFileSync(pathFiles + 'SRN 00000225.cdf');
+            netcdfGcms.bind(null, data).should.throw('Finnigan format not implemented yet');
+        });
+
+        /*
+        it('fromFinnigan', function () {
+            const data = fs.readFileSync(pathFiles + 'SRN 00000225.cdf');
+            netcdfGcms.bind(null, data).should.throw('Finnigan format not implemented yet');
+        });
+        */
     });
 });
